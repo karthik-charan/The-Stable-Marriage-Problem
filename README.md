97bb667f26717d3651d27df6cf3dbf1d9d1df5d0
Karthik Charan  Raghunathan <karthik-charan@Karthiks-MacBook-Pro.local>
2020-09-09 19:48:12 +0530

    Code Checkin

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # The-Stable-Marriage-Problem
-My algorithm for solving the stable marriage problem.
+
+The Stable marriage prolem is one of the most important problem when it comes to algorithms, but often the solution on the internet seems to be quite confusing. Hence I decided to pulish my own version of solving the problem which I find relatively easy to understand. Feel free to use this to schedule your next blind date within your friends circle :p, or to assign the issues to developers within your organisation according to their preference so that everyone gets to enjoy their work!
+
+###Problem:
+The stable marriage problem is defined as follows:
+Suppose you have N men and N women, and each person has ranked their prospective opposite-sex partners in order of preference.
+For example, if N = 3, the input could be something like this:
+
+guy_preferences = { 'andrew': ['caroline', 'abigail', 'betty'], 
+                    'bill': ['caroline', 'betty', 'abigail'], 
+                    'chester': ['betty', 'caroline', 'abigail'], } 
+                    
+gal_preferences = { 'abigail': ['andrew', 'bill', 'chester'], 
+                    'betty': ['bill', 'andrew', 'chester'], 
+                    'caroline': ['bill', 'chester', 'andrew'] }
+
+Write an algorithm that pairs the men and women together in such a way that no two people of opposite sex would both rather be with each other than with their current partners.
---
+++ code.py
@@ -0,0 +1,63 @@
+#### Complete code for Stable Marriage Problem
+## Declaring the required datastructures
+guy_preferences = {
+    'andrew': ['caroline', 'abigail', 'betty'],
+    'bill': ['caroline', 'betty', 'abigail'],
+    'chester': ['betty', 'caroline', 'abigail'],
+}
+
+gal_preferences = {
+    'abigail': ['andrew', 'bill', 'chester'],
+    'betty': ['bill', 'andrew', 'chester'],
+    'caroline': ['bill', 'chester', 'andrew']
+}
+
+guy_gal = {}
+guy_gal_sum = {}
+
+## Initializing the Datastructures
+for i in guy_preferences:
+    guy_gal[i]=''
+    guy_gal_sum[i] = 0
+for j in gal_preferences:
+    guy_gal[j]=''
+    guy_gal_sum[j] = 0
+
+## Defining required methods    
+def new_over_old(woman, man_old, man_new):
+    for i in gal_preferences[woman]:
+        if i==man_old:
+            return False
+        elif i==man_new:
+            return True
+        
+def stable_marriage_algo():
+    while sum(list(guy_gal_sum.values()))<len(guy_gal):
+        for i in guy_preferences:
+            z = 0
+            while z<len(guy_preferences[i]):
+                
+                if sum(list(guy_gal_sum.values()))==len(guy_gal):
+                    break
+                    
+                if new_over_old(guy_preferences[i][z],guy_gal[guy_preferences[i][z]],i):
+                    
+                    guy_gal[guy_preferences[i][z]]=''
+                    guy_gal_sum[guy_preferences[i][z]]=0
+
+                    guy_gal[guy_preferences[i][z]]=i
+                    guy_gal[i]=guy_preferences[i][z]
+                    guy_gal_sum[i] = 1
+                    guy_gal_sum[guy_preferences[i][z]]=1
+
+                    break
+
+                else:
+                    z+=1
+    
+    return guy_gal
+
+##main function
+if __name__ == "__main__":
+    print("The most stable arrangement of couples: ")
+    print(stable_marriage_algo())
